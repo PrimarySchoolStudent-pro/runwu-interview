a780cfd5c8096c93e4b0db81801e694397438ea5
shenglong.gu <2803434655@qq.com>
2024-07-29 21:24:22 +0800

    runwu interview

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .custom_serialziers import SignUpSerializer, UserSerializer
+
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                UserSerializer(user).data,
+                status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignInView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = User.objects.filter(email=email).first()
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh_token': str(refresh),
+                'access_token': str(refresh.access_token),
+            })
+        return Response({'error': '无效的凭证'},
+                        status=status.HTTP_401_UNAUTHORIZED)
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
